a736aa2089017fe43b299d115ddc2fe3adb8e917
Zach Kelling <z@zeekay.io>
2017-04-09 22:15:19 -0500

    Slight refactor

--- cookies.js
+++ cookies.js
@@ -204,79 +204,36 @@ Cookies = (function() {
     this.defaults = defaults != null ? defaults : {};
     this.get = (function(_this) {
       return function(key) {
-        return _this.api(key);
+        return _this.read(key);
+      };
+    })(this);
+    this.getJSON = (function(_this) {
+      return function(key) {
+        var err;
+        try {
+          return JSON.parse(_this.read(key));
+        } catch (error) {
+          err = error;
+          return {};
+        }
       };
     })(this);
     this.remove = (function(_this) {
       return function(key, attrs) {
-        return _this.api(key, '', index$1({
+        return _this.write(key, '', index$1({
           expires: -1
         }, attrs));
       };
     })(this);
     this.set = (function(_this) {
       return function(key, value, attrs) {
-        return _this.api(key, value, attrs);
-      };
-    })(this);
-    this.getJSON = (function(_this) {
-      return function(key) {
-        var err, val;
-        val = _this.api(key);
-        if (val == null) {
-          return {};
-        }
-        try {
-          return JSON.parse(val);
-        } catch (error) {
-          err = error;
-          return val;
-        }
+        return _this.write(key, value, attrs);
       };
     })(this);
   }
 
-  Cookies.prototype.api = function(key, value, attrs) {
-    var attr, cookie, cookies, err, expires, i, kv, len, name, parts, rdecode, result, strAttrs;
-    if (typeof document === 'undefined') {
-      return;
-    }
-    if (arguments.length > 1) {
-      attrs = index$1({
-        path: '/'
-      }, this.defaults, attrs);
-      if (isNumber(attrs.expires)) {
-        expires = new Date;
-        expires.setMilliseconds(expires.getMilliseconds() + attrs.expires * 864e+5);
-        attrs.expires = expires;
-      }
-      attrs.expires = attrs.expires ? attrs.expires.toUTCString() : '';
-      try {
-        result = JSON.stringify(value);
-        if (/^[\{\[]/.test(result)) {
-          value = result;
-        }
-      } catch (error) {
-        err = error;
-      }
-      value = encodeURIComponent(String(value)).replace(/%(23|24|26|2B|3A|3C|3E|3D|2F|3F|40|5B|5D|5E|60|7B|7D|7C)/g, decodeURIComponent);
-      key = encodeURIComponent(String(key));
-      key = key.replace(/%(23|24|26|2B|5E|60|7C)/g, decodeURIComponent);
-      key = key.replace(/[\(\)]/g, escape);
-      strAttrs = '';
-      for (name in attrs) {
-        attr = attrs[name];
-        if (!attr) {
-          continue;
-        }
-        strAttrs += '; ' + name;
-        if (attr === true) {
-          continue;
-        }
-        strAttrs += '=' + attr;
-      }
-      return document.cookie = key + '=' + value + strAttrs;
-    }
+  Cookies.prototype.read = function(key) {
+    var cookie, cookies, err, i, kv, len, name, parts, rdecode, result;
     if (!key) {
       result = {};
     }
@@ -303,6 +260,44 @@ Cookies = (function() {
       }
     }
     return result;
+  };
+
+  Cookies.prototype.write = function(key, value, attrs) {
+    var attr, err, expires, name, result, strAttrs;
+    attrs = index$1({
+      path: '/'
+    }, this.defaults, attrs);
+    if (isNumber(attrs.expires)) {
+      expires = new Date;
+      expires.setMilliseconds(expires.getMilliseconds() + attrs.expires * 864e+5);
+      attrs.expires = expires;
+    }
+    attrs.expires = attrs.expires ? attrs.expires.toUTCString() : '';
+    try {
+      result = JSON.stringify(value);
+      if (/^[\{\[]/.test(result)) {
+        value = result;
+      }
+    } catch (error) {
+      err = error;
+    }
+    value = encodeURIComponent(String(value)).replace(/%(23|24|26|2B|3A|3C|3E|3D|2F|3F|40|5B|5D|5E|60|7B|7D|7C)/g, decodeURIComponent);
+    key = encodeURIComponent(String(key));
+    key = key.replace(/%(23|24|26|2B|5E|60|7C)/g, decodeURIComponent);
+    key = key.replace(/[\(\)]/g, escape);
+    strAttrs = '';
+    for (name in attrs) {
+      attr = attrs[name];
+      if (!attr) {
+        continue;
+      }
+      strAttrs += '; ' + name;
+      if (attr === true) {
+        continue;
+      }
+      strAttrs += '=' + attr;
+    }
+    return document.cookie = key + '=' + value + strAttrs;
   };
 
   return Cookies;
